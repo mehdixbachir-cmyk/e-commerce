67203c13895a6e2c7737e351dc8447aed55c443a
bachir <Mehdixbachir@gmail.com>
2026-05-25 19:02:05 +0100

    Etape 1 - integration des templates

---
+++ src/Controller/ShopController.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ShopController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('shop/index.html.twig');
+    }
+
+    #[Route('/login', name: 'app_login')]
+    public function login(): Response
+    {
+        return $this->render('shop/login.html.twig');
+    }
+
+    #[Route('/profile', name: 'app_profile')]
+    public function profile(): Response
+    {
+        return $this->render('shop/profile.html.twig');
+    }
+
+    #[Route('/categories', name: 'app_categories')]
+    public function categories(): Response
+    {
+        return $this->render('shop/browse_categories.html.twig');
+    }
+
+    #[Route('/cart', name: 'app_cart')]
+    public function cart(): Response
+    {
+        return $this->render('shop/cart.html.twig');
+    }
+}
